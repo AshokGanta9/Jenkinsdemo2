e4768a4b3aa88d0ca54f6dd11fb71235861875c4
Dhanush Ganta <ashokganta40@gmail.com>
2023-01-10 13:08:18 +0530

    class3 is commited

---
+++ class3.java
@@ -0,0 +1,14 @@
+package App2;
+
+public class class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class3 c3= new class3();
+		c3.display();
+
+	}
+	public void display() {
+		System.out.println("Hello Jenkins");
+	}
+}
