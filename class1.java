b34f546a27ce3104a5e25d1288435ad19a6d7882
Dhanush Ganta <ashokganta40@gmail.com>
2023-01-10 12:59:36 +0530

    class1 is commited

---
+++ class1.java
@@ -0,0 +1,15 @@
+package App2;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class1 c1= new class1();
+		c1.display();
+		
+
+	}
+	public void display() {
+		System.out.println("Hello World");
+	}
+}
