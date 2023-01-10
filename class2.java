9d373749f99961d2561e3c8fab0f978874dee802
Dhanush Ganta <ashokganta40@gmail.com>
2023-01-10 13:07:44 +0530

    class2 is commited

---
+++ class2.java
@@ -0,0 +1,13 @@
+package App2;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class2 c2 = new class2();
+		c2.display();
+	}
+	public void display() {
+		System.out.println("Hello Wipro");
+	}
+}
